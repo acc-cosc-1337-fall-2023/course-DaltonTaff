994e9aaa2935f9cc3815a94e0e7d194180a69a37
Dalton Tafolla <98137739+DaltonTaff@users.noreply.github.com>
2023-08-30 00:49:36 +0000

    intro to dev process, int, unit tests

--- src/examples/01_data_types/01_int/main.cpp
+++ src/examples/01_data_types/01_int/main.cpp
@@ -1,5 +1,18 @@
+#include<iostream>
+#include"int.h" // i have access to the echo_variable function in my library
+
+using std::cout;
 int main()
 {
+    int num = 5;//Declare the num variable to be of the type int(whole numbers) and we asign the value 5
 
+    int result = echo_variable(num);
+
+    cout<<"Result: "<<result<<"\n";
+    cout<<"Num: " <<num<<"\n";
+
+    num = 100;//asign the value of 100
+    cout<<"Num:  "<<num<<"\n";
+    
     return 0;
 }
