7aaa98737988a7b6c302b468c614bb4906cab758
Dalton Tafolla <98137739+DaltonTaff@users.noreply.github.com>
2023-09-09 16:12:55 +0000

    Homework 2

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,11 +1,40 @@
 ﻿//write include statements
+#include <iostream>
+#include "hwexpressions.h"
+using namespace std;
 
 
 //write namespace using statement for cout
 
 int main()
 {
-	
+	//variables
+
+	double mealamount, tiprate, total = 0;
+
+	//getting user input
+
+	cout<<"How much was your meal :";
+	cin>>mealamount;
+
+	cout<<"Enter desired tiprate :";
+	cin>>tiprate;
+
+	//Tax amount
+
+	double taxamount = get_sales_tax_amount(mealamount);
+
+	//Tip amount
+
+	double tipamount = get_tip_amount(mealamount,tiprate);
+
+	total = mealamount + tipamount + taxamount;
+
+	//Output
+	cout<<"Meal Amount :$"<<mealamount<<endl;
+	cout<<"Sales Tax :$"<<taxamount<<endl;
+	cout<<"Tip Amount :$"<<tipamount<<endl;
+	cout<<"Total :$"<<total;
 
 	return 0;
 }
